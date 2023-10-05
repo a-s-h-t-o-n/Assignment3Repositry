59abbba91040460c1b1e102f509bad1bbf6d7285
Ashton <ashton.king@student.uts.edu.au>
2023-10-05 16:30:59 +1100

    added player death spritesheet

--- Assets/Scripts/RotatePlayer.cs
+++ Assets/Scripts/RotatePlayer.cs
@@ -15,22 +15,22 @@ public class RotatePlayer : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            this.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270.0f);
         }
 
 
