6e11c12c0386fbf7578173265753c55e4eef984e
Ashton <ashton.king@student.uts.edu.au>
2023-10-06 17:57:30 +1100

    created script to move and rotate player

--- Assets/Scripts/AutoMovePlayer.cs
+++ Assets/Scripts/AutoMovePlayer.cs
@@ -5,14 +5,46 @@ using UnityEngine;
 public class AutoMovePlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+    float startTime;
+
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < 6.0f && transform.position.y == 0.0f)
+        {
+            lerpMethod(new Vector3(0.0f, 0.0f, 0.0f), new Vector3(6.0f, 0.0f, 0.0f), 3.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+        }
+        else if (transform.position.x == 6.0f && transform.position.y > -5.0f)
+        {
+            lerpMethod(new Vector3(6.0f, 0.0f, 0.0f), new Vector3(6.0f, -5.0f, 0.0f), 3.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 180.0f);
+        }
+        else if (transform.position.x > 0.0f && transform.position.y == -5.0f)
+        {
+            lerpMethod(new Vector3(6.0f, -5.0f, 0.0f), new Vector3(0.0f, -5.0f, 0.0f), 3.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 270.0f);
+        }
+        else if (transform.position.x == 0.0f && transform.position.y < 0.0f)
+        {
+            lerpMethod(new Vector3(0.0f, -5.0f, 0.0f), new Vector3(0.0f, 0.0f, 0.0f), 3.0f);
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        }
+
+        if (transform.position == new Vector3(0.0f, 0.0f, 0.0f) || transform.position == new Vector3(6.0f, 0.0f, 0.0f) || transform.position == new Vector3(6.0f, -5.0f, 0.0f) || transform.position == new Vector3(0.0f, -5.0f, 0.0f))
+        {
+            startTime = Time.time;
+            
+        }
+    }
+
+    void lerpMethod(Vector3 StartPos, Vector3 EndPos, float duration)
+    {
+        transform.position = Vector3.Lerp(StartPos, EndPos, (Time.time - startTime) / duration);
     }
 }
